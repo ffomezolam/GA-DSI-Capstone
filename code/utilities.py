5e81f40ca795cb24bd3d5e204febb2bc9f069209
Andrew Malozemoff <ffomezolam@gmail.com>
2022-10-10 14:05:44 -0400

    reoarganize data, updated notebooks

---
+++ code/utilities.py
@@ -0,0 +1,48 @@
+""" word_deconstructor.py
+-------------------------
+Operations for deconstructing text into words
+"""
+
+import re
+
+RE_WORD = re.compile(r"\b[\w'’]+\b")
+RE_WHITESPACE = re.compile(r'\s+')
+RE_SENTENCE = re.compile(r'\w[\w\s,:;\'’-]+[.?!]')
+
+def extract_words(text, n=1):
+    return RE_WORD.findall(text)
+
+def extract_sentences(text):
+    return RE_SENTENCE.findall(text)
+
+
+###------------------------------------------------------------- SELF-TEST
+
+if __name__ == '__main__':
+    import lorem
+    import sys
+
+    def separate():
+        print(f'\n --- \n')
+
+    if len(sys.argv) > 1:
+        # argument
+        with open(sys.argv[1], 'r') as f:
+            text = f.read()
+
+        if len(sys.argv) > 2:
+            n = int(sys.argv[2])
+            if n < 0:
+                text = text[n:]
+            else:
+                text = text[:n]
+    else:
+        text = ' '.join([lorem.paragraph() for _ in range(4)])
+
+    separate()
+    print(text)
+    separate()
+    print(extract_words(text))
+    separate()
+    print(extract_sentences(text))
+    separate()
