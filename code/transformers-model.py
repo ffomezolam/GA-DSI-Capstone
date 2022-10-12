d49c508947898985e85a7d49898fff1633628cb5
Andrew Malozemoff <ffomezolam@gmail.com>
2022-10-12 14:05:38 -0400

    added transformers model script

---
+++ code/transformers-model.py
@@ -0,0 +1,29 @@
+""" transformers-model.py
+-------------------------
+Script to fit a gpt2 transformers model. Intended for use via cloud computing.
+"""
+
+from datasets import Dataset, DatasetDict
+from sklearn.model_selection import train_test_split
+from transformers import AutoTokenizer
+from transformers import DataCollatorForLanguageModeling
+from transformers import TFAutoModelForCausalLM, AdamWeightDecay
+
+from itertools import chain
+import os
+import random
+import re
+import json
+import sys
+
+MODEL_TYPE = 'gpt2-medium'
+MODEL_NAME = sys.argv[1] if len(sys.argv) > 1 else 'test'
+MODEL_DIR = './models/'
+DATA_DIR = './data/'
+
+RE_SENTENCE = re.compile(r'\w.*?[.?!:;]', re.S)
+RE_WHITESPACE = re.compile(r'\s+')
+RE_BLANKLINE = re.compile(r'^\s*$')
+
+END_OF_LINE_TOKEN = '<|eol|>'
+END_OF_SENTENCE_TOKEN = '<|eos|>'
